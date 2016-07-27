23cc418a1657232e05d650346aa9c0f5310a8ec6
Jeremy <jkaffrey@gmail.com>
2016-07-27 09:51:26 -0600

    Done

--- car.js
+++ car.js
@@ -1,9 +1,35 @@
-
+'use strict';
 
 var Car = function (mpg) {
+
+  this.milesPerGal = mpg;
+  this.gallons = 0;
+  this.odme = 0;
+  this.tripThing = [];
+};
+
+Car.prototype.trips = function() {
+
+  return this.tripThing;
+};
+
+Car.prototype.odometer = function() {
+
+  return this.odme;
+};
+
+Car.prototype.drive = function (distance) {
+
+  this.gallons -= (distance / this.milesPerGal);
+  this.odme += distance;
+  this.tripThing.push(distance + ' miles');
+  return this.gallons;
 };
 
 Car.prototype.fill = function (gallons) {
+
+  this.gallons += gallons;
+  return gallons;
 };
 
 module.exports = Car;
